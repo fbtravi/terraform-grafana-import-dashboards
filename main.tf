8a7279b4a760024389b3c7d8b8dea1d657aa21f5
Felipe Travi <felipe.travi@g.globo>
2023-06-08 19:47:41 -0300

    Config_module

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "grafana" {
+  alias  = "my_org"
+  url    = var.grafana_url
+  auth   = var.grafana_auth
+  org_id = var.grafana_org_id
+}
+
+resource "grafana_folder" "folder" {
+  provider = grafana.my_org
+  title    = var.grafana_folder
+}
+
+resource "grafana_dashboard" "dashboard-" {
+  for_each    = var.dashboards
+  provider    = grafana.my_org
+  org_id      = var.grafana_org_id
+  folder      = grafana_folder.folder.uid
+  config_json = file(each.key)
+}
