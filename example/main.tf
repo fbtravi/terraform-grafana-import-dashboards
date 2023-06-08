e0b74e305f9aa3340fdbe25b670e6f043e3f1809
Felipe Travi <felipe.travi@g.globo>
2023-06-08 19:48:20 -0300

    add_example

---
+++ example/main.tf
@@ -0,0 +1,16 @@
+locals {
+  grafana_url    = "http://localhost:3000"
+  grafana_auth   = "admin:grafana"
+  grafana_org_id = 1
+  dashboards     = ["dashboards/dash.json", "dashboards/dash2.json"]
+}
+
+
+module "terraform-grafana-import-dashboards" {
+  source         = "../"
+  grafana_url    = local.grafana_url
+  grafana_auth   = local.grafana_auth
+  grafana_org_id = local.grafana_org_id
+  grafana_folder = "Folder 1"
+  dashboards     = local.dashboards
+}
