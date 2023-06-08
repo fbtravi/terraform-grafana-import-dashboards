a3c1ac9f189ae31edf3bd7631f378a7704963209
Felipe Travi <felipe.travi@g.globo>
2023-06-08 19:49:21 -0300

    add_outputs_folder

---
+++ outputs.tf
@@ -0,0 +1,22 @@
+output "folder_title" {
+  value       = grafana_folder.folder.title
+  description = "Project roles."
+}
+
+output "folder_url" {
+  value       = grafana_folder.folder.url
+  description = "Project roles."
+}
+
+output "folder_id" {
+  value       = grafana_folder.folder.id
+  description = "Project roles."
+}
+
+output "folder_uid" {
+  value       = grafana_folder.folder.uid
+  description = "Project roles."
+}
+
+
+
